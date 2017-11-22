77f743682a7f6903d2e7a9d3550f1075d62d3a90
vinstce <vinstce@gmail.com>
2017-11-21 23:43:16 -0500

    Vinodh Bot initial changes

---
+++ VinodhBot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=VinodhBot-Web-Deploy.pubxml -p:Password=d4NWMCFSMi0prJKls5qj2pDt4Be8EZpDMiWYGjuDvYgZ4diKR5wsupTTew60
+
